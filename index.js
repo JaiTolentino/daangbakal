c80ac37393ddd370ba768b26a712063f08d9972b
JaiTolentino <121657096+JaiTolentino@users.noreply.github.com>
2023-05-05 07:33:42 +0000

    merged post and get requests

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const pool = require('./routes/dbconfig');
-const httppostreq = require('./routes/postreqs');
-const httpgetreq = require('./routes/getreqs');
+const httprequests = require('./routes/httprequests');
 
 pool.connect((error) => {
     if(error) console.log("Failed to Connect: " + error)
@@ -16,8 +15,7 @@ app.use(express.json());
 
 app.use(express.static('views'));
 app.set('view engine', 'hbs');
-app.use('/', httpgetreq);
-app.use(httppostreq);
+app.use('/', httprequests);
 
 app.listen(5000, () => {
     console.log("Started on port 5000");
